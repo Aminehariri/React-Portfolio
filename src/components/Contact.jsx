b5a0bed25fb662975f748fcbb57cb07698f01570
=amine <aminehariri57@gmail.com>
2022-10-25 22:55:07 +0100

    add the emailjs to receve the messages from the contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,29 @@ import {GiNetworkBars} from 'react-icons/gi'
 import {FaMapMarkerAlt} from 'react-icons/fa'
 import {FiMail} from 'react-icons/fi'
 import {BsLinkedin} from 'react-icons/bs'
+import  { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
 
 export default function Contact() {
+
+  const form =useRef();
+
+  const sendEmail = (e) => {
+    
+    e.preventDefault();
+  
+    emailjs.sendForm('service_nwit2b6', 'template_3osw4lr', form.current, 'Hb8usYJJxFW8p6O1Z')
+    e.target.reset()
+    alert("message send")
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      }); 
+  };
+  
+
   return (
     <div name="Contact" className='w-full my-14'>
         <h2 className='text-2xl text-gradient section-title mt-4 mb-12 '> Contact me</h2>
@@ -23,14 +44,14 @@ export default function Contact() {
 
 
           <div className='md:mx-4 md:mr-4'>
-            <form action="" className='w-full'>
+            <form ref={form} onSubmit={sendEmail} className='w-full'>
               <div className='flex flex-wrap my-1 justify-between'>
-                <input type="text"  required className=' inp w-full md:w-[45%] my-5' placeholder='Full Name'/>
-                <input type="text" required className='inp w-full md:w-[45%] md:my-5 mb-5 ' placeholder='Subject' />
+                <input type="text" name='name'  required className=' inp w-full md:w-[45%] my-5' placeholder='Full Name'/>
+                <input type="text" name='subject' required className='inp w-full md:w-[45%] md:my-5 mb-5 ' placeholder='Subject' />
               </div>
               
-              <input type="mail" className='inp w-full mb-5' placeholder='Example@email.com' />
-              <textarea  required className='inp w-full h-[120px] mb-5' placeholder='Message'></textarea>
+              <input type="mail" name='email' className='inp w-full mb-5' placeholder='Example@email.com' />
+              <textarea  required name='message' className='inp w-full h-[120px] mb-5' placeholder='Message'></textarea>
               <input type="submit" className='border border-[#4ca5ff] p-3 cursor-pointer text-white' value='send message' name="" id="" />
             </form>
           </div>
@@ -38,6 +59,7 @@ export default function Contact() {
     </div>
   )
 }
+
 
 
 
